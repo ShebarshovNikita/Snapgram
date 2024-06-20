e28d3edfecd87f076954b0c00c57907d13a8766e
NikitaSergeevich <nvpetrochenko@edu.hse.ru>
2024-06-20 18:19:51 +0300

    setup project

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Loader = () => {
+  return (
+    <div className='flex-center w-full'>
+      <img
+        src='icons/loader.svg'
+        alt='loader'
+        width={24}
+        height={24}
+      />
+    </div>
+  )
+}
